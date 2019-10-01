7f743a9dcc5174f316e3f40c9ad42d70de525c71
Aki Sasaki <aki@escapewindow.com>
2019-09-30 19:05:26 -0700

    build kind (#5)
    * test xpi-manifest.yml
    
    * xpi-type
    
    * taskgraph doesn't throw exception

--- taskcluster/xpi_taskgraph/build.py
+++ taskcluster/xpi_taskgraph/build.py
@@ -7,34 +7,39 @@ kind.
 """
 
 from __future__ import absolute_import, print_function, unicode_literals
+from copy import deepcopy
+import os
 
 from taskgraph.transforms.base import TransformSequence
+from xpi_taskgraph.xpi_manifest import get_manifest
 
 
 transforms = TransformSequence()
 
 
 @transforms.add
-def add_artifacts(config, tasks):
-    for task in tasks:
-        variant = task["attributes"]["build-type"]
-        variant_config = {"xpis": []}  # TODO
-        artifacts = task.setdefault("worker", {}).setdefault("artifacts", [])
-        task["attributes"]["xpis"] = xpis = {}
-        if "xpi-artifact-template" in task:
-            artifact_template = task.pop("xpi-artifact-template")
-            for xpi in variant_config["xpis"]:
-                xpi_name = artifact_template["name"].format(
-                    variant=variant, **xpi
-                )
-                artifacts.append(
-                    {
-                        "type": artifact_template["type"],
-                        "name": xpi_name,
-                        "path": artifact_template["path"].format(
-                            variant=variant, **xpi
-                        ),
-                    }
-                )
+def tasks_from_manifest(config, jobs):
+    manifest = get_manifest()
+    for job in jobs:
+        for xpi_config in manifest.get("xpis", []):
+            if not xpi_config.get("active"):
+                continue
+            task = deepcopy(job)
+            task.setdefault("env", {})
+            task["env"]["XPI_SOURCE_REPO"] = xpi_config["repo"]
+            task["label"] = "build-{}".format(xpi_config["name"])
+            if xpi_config.get("directory"):
+                task["env"]["XPI_SOURCE_DIR"] = xpi_config["directory"]
+            if xpi_config.get("private-repo"):
+                task["secrets"] = [config["github_clone_secret"]]
+            task.setdefault("attributes", {})["addon-type"] = xpi_config["addon-type"]
+            artifacts = task.setdefault("worker", {}).setdefault("artifacts", [])
+            for artifact in xpi_config["artifacts"]:
+                artifact_name = os.path.basename(artifact),
+                artifacts.append({
+                    "type": "file",
+                    "name": "public/build/{}".format(artifact_name),
+                    "path": artifact,
+                })
 
-        yield task
+            yield task
