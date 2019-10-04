c2f67aa55b67ee54abeea7ed4010db0d4b981f5c
Aki Sasaki <aki@escapewindow.com>
2019-10-04 12:42:13 -0700

    build.sh changes

--- taskcluster/docker/node/build.sh
+++ taskcluster/docker/node/build.sh
@@ -1,58 +1,12 @@
 #!/bin/bash
 set -o errexit -o pipefail
 
-test_is_subdir() {
-  local parent_dir=$(realpath "$1")
-  local target_dir=$(realpath "$2")
-  len1=$(echo $parent_dir | wc -c)
-  if [[ "${target_dir:0:$len1}" != ${parent_dir}/ ]]; then
-    echo "Bad parent dir: $2 is not under $1"
-    exit 1
-  fi
-}
+export XPI_SOURCE_REPO=https://github.com/mozilla/shield-studies-addon-utils
+export XPI_NAME=shield-study-helper-addon
+export XPI_TYPE=standard
+export XPI_SOURCE_DIR=misc/shield-study-helper-addon
+export ARTIFACT_PREFIX=public/build
+export XPI_INSTALL_TYPE=npm
+export XPI_ARTIFACTS=linked-addon.xpi
 
-test_var_set() {
-  local varname=$1
-
-  if [[ -z "${!varname}" ]]; then
-    echo "error: ${varname} is not set"
-    exit 1
-  fi
-}
-
-test_var_set XPI_SOURCE_REPO
-test_var_set XPI_ARTIFACTS
-test_var_set ARTIFACT_PREFIX
-
-ARTIFACT_DIR=/builds/worker/artifacts
-SOURCE_DIR=/builds/worker/checkouts/xpi-source
-ARTIFACT_PREFIX_DIR="${ARTIFACT_DIR}/${ARTIFACT_PREFIX}"
-mkdir -p "${ARTIFACT_PREFIX_DIR}"
-test_is_subdir "${ARTIFACT_DIR}" "${ARTIFACT_PREFIX_DIR}"
-
-# TODO set XPI_SOURCE_SECRET_NAME in private repos
-if [[ -z "${!XPI_SOURCE_SECRET_NAME}" ]]; then
-  cd /builds/worker/checkouts
-  git clone "${XPI_SOURCE_REPO}" xpi-source
-else
-  # TODO private repo clone
-  echo "Private repos aren't supported yet!"
-  exit 1
-fi
-
-cd "${SOURCE_DIR}"
-if [[ -z "${XPI_SOURCE_DIR}" ]]; then
-  test_is_subdir "${SOURCE_DIR}" "${XPI_SOURCE_DIR}"
-  cd "${XPI_SOURCE_DIR}"
-fi
-
-if [[ "${XPI_INSTALL_TYPE:-yarn}" == "yarn" ]]; then
-  yarn install --frozen-lockfile
-else
-  npm install
-fi
-
-yarn build
-
-# create manifest
-# copy artifacts
+python3 build.py
