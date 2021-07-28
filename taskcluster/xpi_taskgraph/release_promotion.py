c76da6d9f294f8626d6778d42c32c2ed2e70fb5c
Joel Maher <jmaher@mozilla.com>
2021-07-28 13:45:14 -0700

    sync up from master

--- taskcluster/xpi_taskgraph/release_promotion.py
+++ taskcluster/xpi_taskgraph/release_promotion.py
@@ -106,14 +106,6 @@ def is_release_promotion_available(parameters):
                 ),
                 "default": [],
             },
-            "additional_shipit_emails": {
-                "type": "array",
-                "description": (
-                    "Optional: an array of email strings that "
-                    "should be notified when releases change status"
-                ),
-                "default": [],
-            },
         },
         "required": ["release_promotion_flavor", "xpi_name", "build_number"],
     },
@@ -176,8 +168,6 @@ def release_promotion_action(parameters, graph_config, input, task_group_id, tas
 
     if input.get("version"):
         parameters["version"] = input["version"]
-
-    parameters["additional_shipit_emails"] = input.get("additional_shipit_emails", [])
 
     parameters["additional_shipit_emails"] = input.get("additional_shipit_emails", [])
 
